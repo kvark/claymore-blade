6517193b8d20d4d981bbd44e49817f10194f9642
Dzmitry Malyshau <kvark@fastmail.com>
2026-08-20 22:05:27 -0700

    gpu: remove pipe_enc lifetime alias (replaced by trait bounds)

--- src/pipe_enc.rs
+++
@@ -1,7 +0,0 @@
-//! Backend-specific PipelineEncoder lifetime arity.
-//! Vulkan/Metal use two lifetimes; GLES/WebGL use one.
-
-#[cfg(any(target_arch = "wasm32", gles))]
-pub type PipeEnc<'a> = blade_graphics::PipelineEncoder<'a>;
-#[cfg(not(any(target_arch = "wasm32", gles)))]
-pub type PipeEnc<'a> = blade_graphics::PipelineEncoder<'a, 'a>;
